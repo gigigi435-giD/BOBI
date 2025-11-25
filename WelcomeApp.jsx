6e465f169159b531309e9565080aab660cb6b58b
gigigi435-giD <alejandrogomas535@gmail.com>
2025-11-25 11:25:28 +0300

    Create WelcomeApp.jsx

---
+++ WelcomeApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function WelcomeApp() {
+  return (
+    <div>
+      <h1>Добро пожаловать!</h1>
+      <p>Это простое React-приложение.</p>
+    </div>
+  );
+}
+
+export default WelcomeApp;
